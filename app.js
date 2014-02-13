b5e2e0916f9e0afec9a3c74e68a28aacb7ab2e2b
Andrew Shaffer <andrew@weo.io>
2014-02-13 12:38:13 -0800

    add port log

--- app.js
+++ app.js
@@ -72,4 +72,6 @@ app.get('/', function(req, res) {
   });
 });
 
-app.listen(process.env.PORT || 5000);
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('listening on port', port);
