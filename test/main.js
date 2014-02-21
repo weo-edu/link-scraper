eeb629528f6137df21e2061a853dae8a129d22bb
Andrew Shaffer <andrew@weo.io>
2014-02-20 18:11:05 -0800

    add some unit tests

--- test/main.js
+++ test/main.js
@@ -2,11 +2,15 @@ var _ = require('lodash')
   , app = require('../')
   , request = require('supertest');
 
+function scrape(url) {
+  return request(app)
+    .get('/?url=' + url)
+    .expect('Content-Type', /json/);
+}
+
 describe('link-scraper', function() {
   it('should parse google.com', function(done) {
-    request(app)
-      .get('/?url=google.com')
-      .expect('Content-Type', /json/)
+    scrape('google.com')
       .expect(200)
       .expect(function(res) {
         if(res.body.title !== 'Google')
@@ -26,9 +30,7 @@ describe('link-scraper', function() {
   });
 
   it('should parse github.com', function(done) {
-    request(app)
-      .get('/?url=github.com')
-      .expect('Content-Type', /json/)
+    scrape('github.com')
       .expect(200)
       .expect(function(res) {
         if(! res.body.title)
@@ -40,6 +42,36 @@ describe('link-scraper', function() {
         // find one
         if(! res.body.image)
           return "No image found on github.com";
+      })
+      .end(function(err, res) {
+        if(err) throw err;
+        done();
+      });
+  });
+
+  it('should parse facebook.com', function(done) {
+    scrape('facebook.com')
+      .expect(200)
+      .expect(function(res) {
+        if(res.body.content !== 'text'
+          || res.body['og:site_name'] !== 'Facebook')
+          return 'Failed to parse facebook.com';
+      })
+      .end(function(err, res) {
+        if(err) throw err;
+        done();
+      });
+  });
+
+  it('should scrape a youtube video', function(done) {
+    scrape('https://www.youtube.com/watch?v=jr9kU1xDa9M')
+      .expect(200)
+      .expect(function(res) {
+        if(res.body['og:site_name'] !== 'YouTube'
+          || res.body['og:title'] !== 'Furmulation'
+          || res.body['og:description'] !== 'Furman'
+          || ! res.body['og:video'])
+          return 'Failed to parse youtube video';
       })
       .end(function(err, res) {
         if(err) throw err;
